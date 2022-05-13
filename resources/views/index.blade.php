428d42f08d962721e3998da7061c77ed919af7d3
thereferee12 <l.episcopo@fm-technology.it>
2022-05-13 13:24:56 +0200

    grafica

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,7 +29,9 @@
 
                     <div class="table-responsive">
                        <!--<table class="table table-bordered table-striped">-->
-                            <table class="table table-striped">
+
+
+                            <table class="table table-striped" id="gare">
 
                                 <thead class="thead-dark">
 
